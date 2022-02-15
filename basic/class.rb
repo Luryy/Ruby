31bd06cdf73665753544dafdb39226d8dc57ff29
Lucas Yuri <lucasyuri56@hotmail.com>
2022-02-15 17:23:04 -0300

    feat: add classes features

--- basic/class.rb
+++ basic/class.rb
@@ -1,8 +1,12 @@
 class Cart
+	attr_accessor :public_name, :public_year
+	# attr_accessor = attr_reader & attr_writer
+
 	@@id = 0
 	def initialize(year, name)
 		@year = year
 		@name = name
+		self.public_year = year # or @public_year
 	end
 
 	def getName
@@ -17,9 +21,21 @@ class Cart
 		puts "#{@name} #{@year} #@@id"
 	end
 
+	def duplicateCar
+		new_car = dup # self.dup is implicity
+	end
+
 end
 
 cart = Cart.new(2021, "lury")
 cart.getName
+cart.public_name = "public lury"
+puts cart.public_name
+puts cart.public_year
 cart.setName("lucas")
+cart.info
+
+new_car = cart.duplicateCar
+new_car.setName("Yuri")
+new_car.info
 cart.info
